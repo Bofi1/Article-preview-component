2aac0af08462a01db7ee16dd6eb911b063df5777
Bofi1 <angelborque03github@gmail.com>
2025-08-25 13:33:32 -0700

    desktop done

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let share1 = document.getElementById("share1");
 let footer1 = document.getElementById("footer1");
 let footer2 = document.getElementById("footer2");
 let share2 = document.getElementsByClassName("share2");
+let shareDesktop = document.getElementById("share-desktop");
+let share2desktop = document.getElementsByClassName("share2desktop");
 
-// everything commented is about copied animation
-// let share3 = document.getElementById("share3");
-// let copy = document.getElementById("copy");
+let shareDesktopButton = document.getElementById("share-desktop-button");
 
 share1.addEventListener("click", () => {
   footer1.classList.add("hidden");
@@ -13,31 +13,22 @@ share1.addEventListener("click", () => {
   footer2.classList.add("flex");
 });
 
-// share3.addEventListener("click", () => {
-//   footer1.classList.remove("hidden");
-//   footer2.classList.add("hidden");
-//   footer2.classList.remove("flex");
-// });
-
 for (let i = 0; i < share2.length; i++) {
   share2[i].addEventListener("click", () => {
     footer1.classList.remove("hidden");
     footer2.classList.add("hidden");
     footer2.classList.remove("flex");
-
-    // copy.classList.remove("opacity-0");
-    // copy.classList.add("opacity-1");
-    // copy.classList.add("top-[49%]");
-
-    // setTimeout(() => {
-    //   copy.classList.add("opacity-0");
-    //   copy.classList.remove("opacity-1");
-    //   copy.classList.add("top-[47%]");
-    // }, 800);
-
-    // setTimeout(() => {
-    //   copy.classList.remove("top-[49%]");
-    //   copy.classList.remove("top-[47%]");
-    // }, 900);
   });
 }
+
+for (let i = 0; i < share2desktop.length; i++) {
+  share2desktop[i].addEventListener("click", () => {
+    shareDesktop.classList.add("hidden");
+    shareDesktop.classList.remove("flex");
+  });
+}
+
+shareDesktopButton.addEventListener("click", () => {
+  shareDesktop.classList.toggle("flex");
+  shareDesktop.classList.toggle("hidden");
+});
